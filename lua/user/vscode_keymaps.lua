24725718a7be6fc2ccb2bdb040b501d67a698e10
Oskar Georgsson <oskar.georgsson@gmail.com>
2025-03-23 17:42:54 +0100

    Added keymap for running debug

--- lua/user/vscode_keymaps.lua
+++ lua/user/vscode_keymaps.lua
@@ -9,6 +9,7 @@ keymap({ "n", "v" }, "<leader>i", "<cmd>lua require('vscode').action('editor.act
 keymap({ "n", "v" }, "<leader>f", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
 keymap({ "n", "v" }, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
 keymap({ "n", "v" }, "<leader>c", "<cmd>lua require('vscode').action('editor.action.commentLine')<CR>")
+keymap({ "n", "v" }, "<leader>rd", "<cmd>lua require('vscode').action('workbench.action.debug.start')<CR>")
 
 -- dotnet
 keymap({ "n", "v" }, "<leader>db", "<cmd>lua require('vscode').action('csdevkit.buildSolution')<CR>")
