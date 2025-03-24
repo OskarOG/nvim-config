aebb8837e7910b764a70b9dac068d17b166f162f
Oskar Georgsson <oskar.g@trygghansa.se>
2025-03-24 10:51:18 +0100

    Project manager keybinds

--- lua/user/vscode_keymaps.lua
+++ lua/user/vscode_keymaps.lua
@@ -37,5 +37,6 @@ keymap({ "n", "v" }, "<leader>h9", "<cmd>lua require('vscode').action('vscode-ha
 
 -- project manager keymaps
 keymap({ "n", "v" }, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
-keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
+keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjects')<CR>")
+keymap({ "n", "v" }, "<leader>pon", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
 keymap({ "n", "v" }, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
